0b268f82803fc74f1d3c6bc321b1032bcde98712
The1Wolfcast <the1wolfcast@gmail.com>
2024-02-15 21:28:10 -0600

    Changed Project Type To Blueprints. No C++

--- Source/MikesSandbox/MikesSandbox.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MikesSandbox.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MikesSandbox, "MikesSandbox" );
