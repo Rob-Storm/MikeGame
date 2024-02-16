7d95f7ea8c9474d549466a2572d3e1fb610e9669
The1Wolfcast <the1wolfcast@gmail.com>
2024-02-15 20:37:46 -0600

    Completely replaced project to remove build errors

---
+++ Source/MikesSandbox/MikesSandbox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MikesSandbox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MikesSandbox, "MikesSandbox" );
